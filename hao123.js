3d2afe26ed33d2622bb3ffacca86decb938f2baf
yayuryan <yayuryan@qq.com>
2015-02-16 20:47:18 +0800

    Update hao123.js

--- hao123.js
+++ hao123.js
@@ -1,1 +1,1 @@
-document.write("<Iframe src=\"http://www.aibaisheng.com\" width=\"100%\" height=\"2200\"   scrolling=\"yes\"  frameborder=\"0\"></Iframe>");
+document.write("<Iframe src=\"http://www.aibaisheng.com\" width=\"100%\" height=\"5200\"   scrolling=\"yes\"  frameborder=\"0\"></Iframe>");
